b5ba4f69ea5bd9fc6f246b1844b21db14fd17108
c2000eric <c2000eric@outlook.com>
2017-08-08 15:22:23 +0800

    Create

---
+++ Day1/Money01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Money01.Startup))]
+namespace Money01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
