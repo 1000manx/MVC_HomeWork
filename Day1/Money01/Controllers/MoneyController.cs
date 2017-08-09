213bc5a1730b367752217d1e9e3d6f3f2042a607
c2000eric <c2000eric@outlook.com>
2017-08-09 17:36:08 +0800

    1.create money index 2.add partial view

---
+++ Day1/Money01/Controllers/MoneyController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Money01.Models;
+using Money01.Models.ViewModels;
+
+namespace Money01.Controllers
+{
+    public class MoneyController : Controller
+    {
+        // GET: Money
+        public ActionResult Index()
+        {            
+            //ViewModel
+            MoneyListViewModel MoneyList = new MoneyListViewModel();
+           
+            MoneyList.MyMoney = DataSource();
+
+
+            return View(MoneyList);
+        }
+
+       
+
+        private List<Money> DataSource()
+        {            
+            List<Money> listMyMoney = new List<Money>();
+            listMyMoney.Add(new Money() { Category = "支出", Date = Convert.ToDateTime("2016-01-01"), Amount = 300, Notes = "早餐" });
+            listMyMoney.Add(new Money() { Category = "支出", Date = Convert.ToDateTime("2016-01-02"), Amount = 1600, Notes = "午餐" });
+            listMyMoney.Add(new Money() { Category = "支出", Date = Convert.ToDateTime("2016-01-03"), Amount = 800, Notes = "晚餐" });
+
+            return listMyMoney;
+        }
+    }
+
+    
+}
